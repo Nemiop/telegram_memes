8cfb66584c7ecd10e5b516f1bdf14b972e778215
Ruslan Timchenko <timchenkoruslan97@gmail.com>
2019-05-18 20:43:09 +0300

    Understood that I live in Ukraine and can't parse Vk without special tools. It is funny story about my 2 days of incomprehension why this doesn't work! :D
    Now I have 1 additional task. 
    0) Learn how to run vk.com

--- vk_parser.py
+++ vk_parser.py
@@ -1,17 +1,27 @@
 import requests
+resp = requests.get('https://www.avito.ru/rossiya/audio_i_video/mp3-pleery?s_trg=11')
+print(resp.json())
 
-token = 'b5bc7a56b5bc7a56b5bc7a5664b5d6e5a7bb5bcb5bc7a56e9628524184d9adee598ff51'
-version = 5.95
-domain = 'science_technology'
-
-BASE_URL = 'https://api.vk.com/method/'
-method = 'wall.get'
-
-url_initial = BASE_URL + method
-
-response = requests.get(url_initial, params={
-'access_token': token,
-'v': version,
-})
-print(response)
-# data = response.json()
+# from httmock import urlmatch, HTTMock
+# import requests
+# import sys
+# sys.stdout.buffer.write('Çàâîäñêàÿ'.encode(sys.stdout.encoding) + b'\n')
+#
+# token = 'b5bc7a56b5bc7a56b5bc7a5664b5d6e5a7bb5bcb5bc7a56e9628524184d9adee598ff51'
+# version = 5.95
+# domain = 'science_technology'
+# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',}
+#
+#
+#
+# BASE_URL = 'https://api.vk.com/method/'
+# method = 'wall.get'
+#
+# url_initial = BASE_URL + method
+#
+# r = requests.get('https://www.avito.ru/rossiya/audio_i_video/mp3-pleery?s_trg=11')
+# d = r.json()
+# print(d)
+# # response = requests.get('https://api.vk.com/method/wall.get?access_token=b5bc7a56b5bc7a56b5bc7a5664b5d6e5a7bb5bcb5bc7a56e9628524184d9adee598ff51&v=5.95&domain=science_technology')
+# #
+# # data = response.json()
