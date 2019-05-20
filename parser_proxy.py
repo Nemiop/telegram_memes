f3b3afa270beef29e319fd96d6abc0860061cbae
Ruslan Timchenko <timchenkoruslan97@gmail.com>
2019-05-20 14:57:05 +0300

    Now it can take 1000 posts from vk group

--- parser_proxy.py
+++ parser_proxy.py
@@ -1,7 +1,6 @@
 # Use dynamic changing Ip and Useragent
 
 import requests
-from bs4 import BeautifulSoup
 
 from random import choice
 from time import sleep
@@ -31,29 +30,3 @@ def get_html_proxy(url, parameters):
         except:
             continue
         return html
-
-
-
-# from httmock import urlmatch, HTTMock
-# import requests
-# import sys
-# sys.stdout.buffer.write('Çàâîäñêàÿ'.encode(sys.stdout.encoding) + b'\n')
-#
-# token = 'b5bc7a56b5bc7a56b5bc7a5664b5d6e5a7bb5bcb5bc7a56e9628524184d9adee598ff51'
-# version = 5.95
-# domain = 'science_technology'
-# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',}
-#
-#
-#
-# BASE_URL = 'https://api.vk.com/method/'
-# method = 'wall.get'
-#
-# url_initial = BASE_URL + method
-#
-# r = requests.get('https://www.avito.ru/rossiya/audio_i_video/mp3-pleery?s_trg=11')
-# d = r.json()
-# print(d)
-# # response = requests.get('https://api.vk.com/method/wall.get?access_token=b5bc7a56b5bc7a56b5bc7a5664b5d6e5a7bb5bcb5bc7a56e9628524184d9adee598ff51&v=5.95&domain=science_technology')
-# #
-# # data = response.json()
--- vk_parser.py
+++ vk_parser.py
@@ -1,28 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
-
+import json
 import parser_proxy as pp
 
-def main():
-    # General informatoin.
-    # URL parameters
+def write_file(data):
+    f = open('vk_html.txt', 'w')
+    f.write(data)
+    f.close()
+
+
+def take_1000_posts(vk_group):
+    # General Information about method and Vk_API
     token = 'b5bc7a56b5bc7a56b5bc7a5664b5d6e5a7bb5bcb5bc7a56e9628524184d9adee598ff51'
     version = 5.95
-    domain = 'science_technology'
     params={
         'access_token': token,
         'v': version,
-        'domain': domain
+        'domain': vk_group,
+        'count': 100
     }
 
     BASE_URL = 'https://api.vk.com/method/'
     method = 'wall.get'
     url = BASE_URL+method
-    # Avoid block Vk.com in Ukraine
-    html = pp.get_html_proxy(url,params)
 
-    print(html.json())
+    offset = 0
+    all_posts = []
 
+    while offset < 1000:
+        params.update({'offset': offset})
+        # Avoid block Vk.com in Ukraine
+        html = pp.get_html_proxy(url, params)
+        data = html.json()['response']['items']
+        all_posts.extend(data)
+
+        offset+=100
+
+    return all_posts
+
+
+def main():
+    vk_group = 'mhkon'
+
+    vk_posts = take_1000_posts(vk_group)
+    # soup = BeautifulSoup()
+    write_file(json.dumps(vk_posts, sort_keys=True, indent=1, separators=(',', ': ')))
 
 
 
